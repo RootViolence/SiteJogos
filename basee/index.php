4ae6116f4b31bccf7f41070fc33ebc7e83e2e3eb
RootViolence <guilhermemendes.trabalho@gmail.com>
2024-06-24 02:14:17 -0300

    Adicionando Bootstrap, carrousel, cards dos jogos

--- basee/index.php
+++ basee/index.php
@@ -5,11 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SK GAMES</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon shortcut" href="imagens/logos/TachPlayINVETIDO.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -18,26 +21,19 @@
 
             <a class="logo" href="home">SK GAMES</a>
 
-            <ul class="nav-list">
+            <ul class="nav-list">   
 
                 <li><a href="equipe">EQUIPE</a></li>
-                <li><a href="">GAMES</a></li>
+                <li><a href="home">GAMES</a></li>
                 <li><a href="contato">CONTATO</a></li>
             </ul>
         </nav>
     </header>
     <main>
         <?php
-
         if (isset($_GET["param"])) {
-
             $param = $_GET["param"];
-
-            //separar o parametro por "/"
             $p = explode("/", $param);
-
-            //print_r($p);
-
         }
 
         $page = $p[0] ?? "home";
@@ -46,16 +42,16 @@
         if ($page == "jogo") {
             $pagina = "jogo/{$jogo}.php";
         } else {
-            $pagina = "paginas/{$page}.php}";
+            $pagina = "paginas/{$page}.php";
         }
 
-        //verificcar se a pagina exite
         if (file_exists($pagina)) {
             include $pagina;
         } else {
             include "paginas/404.php";
         }
         ?>
+
     </main>
 </body>
 <footer>
--- basee/paginas/home.php
+++ basee/paginas/home.php
@@ -1,45 +1,93 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SK GAMES</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-</head>
-<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- STYLESHEET -->
+
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner">
+  <div class="carousel-inner col-8 container mt-1">
     <div class="carousel-item active">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=Primeiro Slide" alt="Primeiro Slide">
+      <img class="d-block w-100" src="imagens/Jogos/sifu.jpeg" alt="Primeiro Slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="imagens/Jogos/imagem1.jpg" alt="Segundo Slide">
+      <img class="d-block w-100" src="imagens/Jogos/imagem2.jpg" alt="Segundo Slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
+      <img class="d-block w-100" src="imagens/Jogos/imagem3.png" alt="Terceiro Slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Anterior</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <a class="carousel-control-next " href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon " aria-hidden="true"></span>
     <span class="sr-only">Próximo</span>
   </a>
 </div>
+<section id="preto">
+<h2 class="container-fluid text-center" style="color:#640b64">MAIS JOGADOS</h2>
+</section>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE">Dark Souls REMASTERED</h5>
+        <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor de gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE">Sifu</h5>
+        <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para PS4, desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu contra os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary"">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE;">Absolver</h5>
+        <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa uma máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias, que criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary" style="color:#BEBEBE;">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
